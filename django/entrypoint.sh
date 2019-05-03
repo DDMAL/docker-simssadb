3f82c859311431303489eb70c0ca299c48e4de33
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2019-05-03 15:41:24 -0400

    Update the script

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -4,6 +4,8 @@ python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-./sample_data/madrigal/add_all.sh
-python manage.py rebuild_index
+cd sample_data
+cd madrigal
+./add_all.sh
+cat <(echo "y") - | python manage.py rebuild_index
 exec "$@"
