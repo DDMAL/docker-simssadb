8ac6e40e2615829f473ba8dad3b4a967d69d720b
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2019-05-03 15:29:57 -0400

    Add commands to pre-populate data and index them

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -4,5 +4,6 @@ python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-
+./sample_data/madrigal/add_all.sh
+python manage.py rebuild_index
 exec "$@"
