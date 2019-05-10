497fe0d38a4780575497c501b5d8fc082e0f561b
deepio <alex.daigle2@gmail.com>
2019-05-10 14:09:03 -0400

    Refactored: django entrypoint

--- django/scripts/entrypoint.sh
+++ django/scripts/entrypoint.sh
@@ -4,10 +4,11 @@ python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-cd sample_data
-cd madrigal
-./add_all.sh
-cd ..
-cd ..
+# Adding Sample Data to the database
+# cd sample_data
+# cd madrigal
+# ./add_all.sh
+# cd ..
+# cd ..
 echo y | python manage.py rebuild_index
 exec "$@"
